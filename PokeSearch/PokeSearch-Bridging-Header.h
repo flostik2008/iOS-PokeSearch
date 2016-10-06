aaa1e4cdefd6f2b78c057ef60479fa136ebe7957
Evgeny Vlasov <564n@Evgenys-MacBook-Pro.local>
2016-10-06 16:19:52 -0700

    Created 2nd VC with UICollectionView

---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PokeSearch-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by Evgeny Vlasov on 10/5/16.
+//  Copyright © 2016 Evgeny Vlasov. All rights reserved.
+//
+
+#import "GeoFire.h"
+
